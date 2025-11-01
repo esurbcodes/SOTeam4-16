32d65c12f72d45c05e69c80180637c947752b3c1
EsauCortez <cortez35@purdue.edu>
2025-10-31 21:45:46 -0400

    Update performance_claims.py

--- src/metrics/performance_claims.py
+++ src/metrics/performance_claims.py
@@ -1,4 +1,6 @@
 from __future__ import annotations
+from huggingface_hub.utils import HfHubHTTPError
+
 # Imports from standard Python libraries
 import time
 import logging
@@ -6,7 +8,6 @@ from typing import Any, Dict, Tuple
 
 # Import the specific functions we need from the huggingface_hub library
 from huggingface_hub import model_info
-from huggingface_hub.utils import HfHubHTTPError
 
 # This gets the same logger instance used by run.py, so all logs go to the same place.
 logger = logging.getLogger("phase1_cli")
