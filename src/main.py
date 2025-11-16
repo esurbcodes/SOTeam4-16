e16f37fb54eda324c3657e644fdc866f5d8118be
CameronToddIU <camerontodd02@gmail.com>
2025-11-15 21:05:48 -0500

    Revert "Update main.py"
    This reverts commit e28a0035f69ca6af0a164c1c132d859fbb5c27a5.

--- src/main.py
+++ src/main.py
@@ -14,13 +14,13 @@ app = FastAPI(title="SOTeam4P2 API")
 <<<<<<< HEAD
 =======
 # During dev you can be permissive:
-origins = ["*"]
+# origins = ["*"]
 
 # Or be specific:
-# origins = [
-#     "http://sot4-model-registry-dev.s3-website.us-east-2.amazonaws.com",
-#     "https://sot4-model-registry-dev.s3-website.us-east-2.amazonaws.com",
-# ]
+origins = [
+    "http://sot4-model-registry-dev.s3-website.us-east-2.amazonaws.com",
+    "https://sot4-model-registry-dev.s3-website.us-east-2.amazonaws.com",
+]
 
 app.add_middleware(
     CORSMiddleware,
